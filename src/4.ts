f1f285b4ef18cf460f793eabbc27dcd3062be2e8
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-10-21 17:31:53 +0300

    Completed

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,69 @@
+
+
+class Key {
+  private signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+
+
+abstract class House {
+  private door: boolean = false;
+  private key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract comeIn(person: Person): void;
+
+  isDoorOpen(): boolean {
+    return this.door;
+  }
+
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+
+  closeDoor(): void {
+    this.door = false;
+  }
+}
+
+
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  comeIn(person: Person): void {
+    if (this.isDoorOpen()) {
+      this.tenants.push(person);
+    }
+  }
+}
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +72,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
